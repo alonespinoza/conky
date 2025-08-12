f36ad8e0f28561eb8cff872aed0960d5c53af985
Alonso Espinoza <alonespinoza@gmail.com>
2025-08-12 09:45:54 -0600

    Set main.lua to load main_bars.lua by default
    - main.lua now automatically loads the working Conky Bars configuration
    - Provides a working Conky setup out of the box
    - Users can run 'conky -c main.lua' and get full functionality
    - Maintains dual-version system while making main.lua the default working version

--- main.lua
+++ main.lua
@@ -1,61 +1,9 @@
 -- ~/.conky/main.lua
--- New Conky configuration (to be built)
--- This is the new version that will be different from conky_bars
+-- Modified to load main_bars.lua by default
+-- This provides a working Conky configuration out of the box
 
 -- Get home directory
 local home = os.getenv('HOME') or '/home/alonso'
 
--- Basic Conky configuration
-conky.config = {
-        -- Window settings
-        own_window = true,
-        own_window_type = 'normal',
-        own_window_transparent = false,
-        own_window_argb_visual = true,
-        own_window_argb_value = 192, -- 25% transparency
-
-        -- Position and size
-        alignment = 'top_right',
-        gap_x = 50,
-        gap_y = 50,
-        minimum_width = 300,
-        minimum_height = 400,
-
-        -- Appearance
-        background = false,
-        border_width = 0,
-        draw_borders = false,
-        draw_graph_borders = false,
-        draw_outline = false,
-        draw_shades = false,
-
-        -- Fonts
-        use_xft = true,
-        font = 'Droid Sans:size=10',
-
-        -- Colors
-        default_color = '#D8DEE9',
-        color1 = '#88C0D0',
-        color2 = '#8FBCBB',
-        color3 = '#81A1C1',
-        color4 = '#5E81AC',
-
-        -- Update settings
-        update_interval = 1,
-        double_buffer = true,
-        no_buffers = true,
-        text_buffer_size = 2048,
-
-        -- Lua settings
-        lua_load = home .. '/.conky/sections/common.lua ' ..
-                home .. '/.conky/sections/system.lua ' ..
-                home .. '/.conky/sections/weather.lua ' ..
-                home .. '/.conky/sections/music.lua'
-}
-
--- Main display text
-conky.text = [[
-${lua_parse system_display}
-${lua_parse weather_display}
-${lua_parse music_display}
-]]
+-- Load the working Conky Bars configuration by default
+dofile(home .. '/.conky/main_bars.lua')
